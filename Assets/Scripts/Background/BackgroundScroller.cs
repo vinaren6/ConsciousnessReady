8e3bafb90d7b1ad34b979c8e5e51b8b17c1dd88d
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2020-11-20 00:53:41 +0100

    First draft on background with parallax, added placeholder for shepherd with new art

---
+++ Assets/Scripts/Background/BackgroundScroller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject virtualCamera;
+
+    public float amountOfParallax = 1f;
+
+    private float textureOffsetX;
+    private float textureOffsetY;
+    private Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+
+    void Update()
+    {
+        textureOffsetX = (virtualCamera.transform.position.x * amountOfParallax) / 10;
+        textureOffsetY = (virtualCamera.transform.position.y * amountOfParallax) / 10;
+        mat.SetTextureOffset("_MainTex", new Vector2(textureOffsetX, textureOffsetY));
+    }
+}
