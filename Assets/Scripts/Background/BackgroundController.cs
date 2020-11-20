deceb7cf6da6aeb06ed41d3a993489e51b4c1fc1
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2020-11-20 12:07:39 +0100

    Tweaks to Background Parallax, Added Background Controller

---
+++ Assets/Scripts/Background/BackgroundController.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+
+    public float speedOfParallax = 60f;
+
+}
---
+++ Assets/Scripts/Background/BackgroundElementScroller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class BackgroundElementScroller : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject virtualCamera;
+
+    [SerializeField]
+    private float amountOfParallax = 1f;
+
+    private float textureOffsetX;
+    private float textureOffsetY;
+
+    private Material mat;
+    private BackgroundController backgroundController;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        GameObject background = GameObject.FindGameObjectWithTag("BackgroundController");
+        backgroundController = background.GetComponent<BackgroundController>();
+    }
+
+
+    void Update()
+    {
+        textureOffsetX = (virtualCamera.transform.position.x * amountOfParallax) / backgroundController.speedOfParallax / 10;
+        textureOffsetY = (virtualCamera.transform.position.y * amountOfParallax) / backgroundController.speedOfParallax / 10;
+        mat.SetTextureOffset("_MainTex", new Vector2(textureOffsetX, textureOffsetY));
+    }
+}
