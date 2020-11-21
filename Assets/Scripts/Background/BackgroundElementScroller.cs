76696faa38b9218cd38acc60a17854281defcb02
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2020-11-21 01:39:22 +0100

    Background exposure tweaks

--- Assets/Scripts/Background/BackgroundElementScroller.cs
+++ Assets/Scripts/Background/BackgroundElementScroller.cs
@@ -9,12 +9,6 @@ public class BackgroundElementScroller : MonoBehaviour
     [SerializeField]
     private float amountOfParallax = 1f;
 
-    [SerializeField]
-    private bool flipX = false;
-
-    [SerializeField]
-    private bool flipY = false;
-
     private float textureOffsetX;
     private float textureOffsetY;
 
@@ -26,9 +20,6 @@ public class BackgroundElementScroller : MonoBehaviour
         mat = GetComponent<Renderer>().material;
         GameObject background = GameObject.FindGameObjectWithTag("BackgroundController");
         backgroundController = background.GetComponent<BackgroundController>();
-
-        //if (flipX)
-            //transform.localScale.x *= -1;
     }
 
 
