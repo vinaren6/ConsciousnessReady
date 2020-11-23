c5f85a195448de4a8d01ec62822d55fd388add52
Viktor Rüdén <viktor.ruden2@gmail.com>
2020-11-23 13:59:31 +0100

    chagne input manager and right trigger on controller changes rotation on player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,9 +8,8 @@ public class PlayerMovement : MonoBehaviour
     private float maxSpeed = 0.1f;
     [SerializeField]
     private float acceleration = 0f;
-    [SerializeField]
-    private float dampening = 0f;
     Vector2 movement;
+    Vector2 moveDirection;
     Rigidbody2D rb2d;
 
     // Start is called before the first frame update
@@ -24,111 +23,32 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        
       
+        moveDirection.x = Input.GetAxisRaw("RightAnalogX") * -1;
+        moveDirection.y = Input.GetAxisRaw("RightAnalogY");
     }
 
     private void FixedUpdate()
     {
-        slowPlayer();
-        Debug.Log(movement + " test 1");
-        Vector2 testMovement = movement.normalized;
-        movement.x = Mathf.Abs(movement.x) * testMovement.x;
-        movement.y = Mathf.Abs(movement.y) * testMovement.y;
-        Debug.Log(movement + " test 2");
+    
         if (rb2d.velocity.magnitude < movement.magnitude * maxSpeed)
             {
-            
-            //velocity += movement.normalized * acceleration * Time.fixedDeltaTime;
+            movement = movement.normalized;
             rb2d.AddForce(movement * acceleration * Time.fixedDeltaTime, ForceMode2D.Impulse);
-            Vector2 moveDirection = rb2d.velocity;
-       
-            if (moveDirection.y < 0.3 && moveDirection.y > -0.3 && moveDirection.x == 0)
-            {
-                moveDirection = Vector2.zero;
+
+            
+
             }
-            if (moveDirection.x < 0.3 && moveDirection.x > -0.3 && moveDirection.y == 0)
-            {
-                moveDirection = Vector2.zero;
-            }
-            if (moveDirection != Vector2.zero )
-            {
-                float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
-                //transform.rotation += 90;
-            }
-            }
-        
-    }
-    private void slowPlayer()
-    {
-        if (movement.y > 0 && rb2d.velocity.y < 0)
+      //  Debug.Log(moveDirection.x);
+        if (Mathf.Abs(moveDirection.x) > 0.4 || Mathf.Abs(moveDirection.y) > 0.4)
         {
-            Vector2 oldMovement = rb2d.velocity;
-            oldMovement.y += dampening * 1.5f;
-            rb2d.velocity = oldMovement;
-        }
-        if (movement.y < 0 && rb2d.velocity.y > 0)
-        {
-            Vector2 oldMovement = rb2d.velocity;
-            oldMovement.y -= dampening * 1.5f;
-            rb2d.velocity = oldMovement;
-        }
-        if (movement.x > 0 && rb2d.velocity.x < 0)
-        {
-            Vector2 oldMovement = rb2d.velocity;
-            oldMovement.x += dampening * 1.5f;
-            rb2d.velocity = oldMovement;
-        }
-        if (movement.x < 0 && rb2d.velocity.x > 0)
-        {
-            Vector2 oldMovement = rb2d.velocity;
-            oldMovement.x -= dampening * 1.5f;
-            rb2d.velocity = oldMovement;
+
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+           // Debug.Log(angle);
+            transform.rotation = Quaternion.AngleAxis(angle + 90, Vector3.forward);
+            //transform.rotation += 90;
         }
 
-
-        if (movement.y == 0)
-        {
-            if (rb2d.velocity.y < 0.1 && rb2d.velocity.y > -0.1)
-            {
-                Vector2 oldMovement = rb2d.velocity;
-                //oldMovement.y = 0;
-               // rb2d.velocity = oldMovement;
-            }
-            if (rb2d.velocity.y > 0)
-            {
-                Vector2 oldMovement = rb2d.velocity;
-                oldMovement.y -= dampening;
-                rb2d.velocity = oldMovement;
-            }
-            if (rb2d.velocity.y < 0)
-            {
-                Vector2 oldMovement = rb2d.velocity;
-                oldMovement.y += dampening;
-                rb2d.velocity = oldMovement;
-            }
-        }
-        if (movement.x == 0)
-        {
-            if (rb2d.velocity.x < 0.1 && rb2d.velocity.x > -0.1)
-            {
-                Vector2 oldMovement = rb2d.velocity;
-                //oldMovement.x = 0;
-                //rb2d.velocity = oldMovement;
-            }
-            if (rb2d.velocity.x > 0)
-            {
-                Vector2 oldMovement = rb2d.velocity;
-                oldMovement.x -= dampening;
-                rb2d.velocity = oldMovement;
-            }
-            if (rb2d.velocity.x < 0)
-            {
-                Vector2 oldMovement = rb2d.velocity;
-                oldMovement.x += dampening;
-                rb2d.velocity = oldMovement;
-            }
-        }
     }
+    
 }
