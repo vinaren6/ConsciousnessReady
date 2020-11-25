a3eec4907684cd62927b28a4acb66a4537baa444
Viktor Rüdén <viktor.ruden2@gmail.com>
2020-11-25 11:20:37 +0100

    playermovement updated with rotation to the direction you are going

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     private float acceleration = 0f;
     Vector2 movement;
     Vector2 moveDirection;
+    Quaternion moveDirectioJoyCon;
     Rigidbody2D rb2d;
 
     // Start is called before the first frame update
@@ -23,9 +24,11 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-      
+
         moveDirection.x = Input.GetAxisRaw("RightAnalogX") * -1;
         moveDirection.y = Input.GetAxisRaw("RightAnalogY");
+
+        
     }
 
     private void FixedUpdate()
@@ -39,11 +42,30 @@ public class PlayerMovement : MonoBehaviour
             
 
             }
-      
+         
         if (moveDirection != Vector2.zero)
         {
+            
             float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle + 90, Vector3.forward);
+            Debug.Log(angle);
+            moveDirectioJoyCon = Quaternion.AngleAxis(angle + 90, Vector3.forward);
+            transform.rotation = moveDirectioJoyCon;
+            Debug.Log(Quaternion.AngleAxis(angle + 90, Vector3.forward).z + "  " + Quaternion.AngleAxis(angle + 90, Vector3.forward).w);
+        }
+
+
+        if (moveDirection == Vector2.zero)
+        {
+            moveDirection = rb2d.velocity;
+           
+
+            if (moveDirection != Vector2.zero)
+            {
+
+                
+                float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+            }
         }
 
     }
