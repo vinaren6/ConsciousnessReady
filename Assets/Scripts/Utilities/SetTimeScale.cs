7e8b3d4a8c634cb2b9ecb29a61c1c562d8624fb2
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2020-11-26 14:56:50 -0800

    added timescale utility

---
+++ Assets/Scripts/Utilities/SetTimeScale.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SetTimeScale : MonoBehaviour
+{
+    [SerializeField]
+    private float timeScale = 1;
+
+    void Start()
+    {
+        Time.timeScale = timeScale;
+    }
+
+}
