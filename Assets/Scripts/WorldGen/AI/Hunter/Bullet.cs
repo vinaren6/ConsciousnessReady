ab63ba9b8f68d327639aee4b75daf61c21587f8e
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2020-11-26 14:29:47 -0800

    AI

---
+++ Assets/Scripts/WorldGen/AI/Hunter/Bullet.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 5;
+    private void FixedUpdate()
+    {
+        transform.Translate(transform.up * Time.fixedDeltaTime * speed);
+    }
+}
