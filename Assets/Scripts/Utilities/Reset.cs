1781d59417059401abedc0fac0e5904a1ae79762
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2020-11-30 10:37:21 -0800

    Create Reset.cs

---
+++ Assets/Scripts/Utilities/Reset.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.R)) {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
