a659c9fdc3ea94f093148fcf14162d7a61420b68
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2020-12-04 13:46:37 -0800

    Update Reset.cs

--- Assets/Scripts/Utilities/Reset.cs
+++ Assets/Scripts/Utilities/Reset.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 public class Reset : MonoBehaviour
 {
     [SerializeField] private InputAction reset;
+
+    private Vector3 pos;
+
     void Start()
     {
         reset.Enable();
-        reset.performed += context => SceneManager.LoadScene(1) ;
+        reset.performed += context => ResetPos();
+
+        pos = PlayerMovement.playerObj.transform.position;
+    }
+    private void ResetPos()
+    {
+        PlayerMovement.playerObj.transform.position = pos;
     }
 }
