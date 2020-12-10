6577ac2081e807e2c555c1790fba19c6e8625123
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2020-12-10 22:56:34 +0100

    Massive update on all prefabs, random animation start frame, fixed all prefabs with empty parent, tweaked ai & health, added green slumber, sorted folder structure

---
+++ Assets/Scripts/Utilities/RandomizeAnimation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor.Animations;
+using UnityEngine;
+
+public class RandomizeAnimation : MonoBehaviour
+{
+    
+    void Start()
+    {
+        GetComponent<Animator>().SetFloat("AnimationOffset", Random.Range(0.0f, 1.0f));
+    }
+
+}
