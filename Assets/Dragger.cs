5ecef4b526be3edb9edfd313d07509e3f612f133
Viktor Rüdén <viktor.ruden2@gmail.com>
2020-12-10 15:20:48 +0100

    made a prototype of dragger

---
+++ Assets/Dragger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragger : MonoBehaviour
+{
+    Vector2 velocity;
+    private bool isCoolided;
+    //[SerializeField] private ScriptableObject
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = GetComponent<Rigidbody2D>().velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = GetComponent<Rigidbody2D>().velocity;
+
+    }
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isCoolided)
+        {
+            Destroy(collision.gameObject.GetComponent<Rigidbody2D>());
+            Destroy(GetComponent<BoxCollider2D>());
+            collision.gameObject.transform.SetParent(transform);
+            if (collision.transform.GetComponent<Dragger>() != null)
+            {
+                Destroy(collision.transform.GetComponent<Dragger>());
+            }
+          
+            isCoolided = true;
+        }
+       // Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
+        GetComponent<Rigidbody2D>().velocity = velocity;
+        //col.gameObject
+        // transform.SetParent(col.transform);
+
+        //Destroy(GetComponent<Rigidbody2D>());
+        //col.transform.parent = transform;
+
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+
+
+    }
---
+++ Assets/Scripts/Interaction/Weapon/DraggerWeapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DraggerWeapon : MonoBehaviour
+{
+    [SerializeField]
+    private float projectileSpeed = 0.001f;
+    [SerializeField] private InputAction shoot;
+    [SerializeField]
+    private Transform gun;
+    [SerializeField]
+    private float coolDown = 1.1f;
+    [SerializeField]
+    private GameObject projectile;
+    private float elapsedTime = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shoot.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (shoot.ReadValue<float>() == 1 && elapsedTime > coolDown)
+        {
+
+            Debug.Log("test");
+            GameObject bullet = Instantiate(projectile, gun.position, transform.rotation) as GameObject;
+            bullet.GetComponent<Rigidbody2D>().AddForce((Vector2)gun.up * projectileSpeed * bullet.GetComponent<Rigidbody2D>().mass, ForceMode2D.Impulse);
+            elapsedTime = 0;
+        }
+    }
+}
