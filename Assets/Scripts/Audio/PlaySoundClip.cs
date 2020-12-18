30f6a1ad643b77320e5a0531c96c8f33834c9763
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2020-12-18 11:26:03 +0100

    Added first draft on UI

---
+++ Assets/Scripts/Audio/PlaySoundClip.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlaySoundClip : MonoBehaviour
+{
+    [SerializeField]
+    private string soundClipName;
+
+    void Start()
+    {
+        FindObjectOfType<AudioManager>().Play(soundClipName);
+    }
+
+}
