a1597c3e5df312c1492c7d2d17a87a43a0a9c77e
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2020-12-18 16:16:42 +0100

    Added temporary tutorial

---
+++ Assets/Scripts/UI/TutorialPlaceholder.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+public class TutorialPlaceholder : MonoBehaviour
+{
+
+
+    private InputActions inputActions;
+    private float numberOfClicks = 0;
+
+    private void Awake()
+    {
+        inputActions = new InputActions();
+
+        inputActions.Player.Shoot.performed +=
+        context =>
+        {
+            StartGame();
+        };
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+
+    void StartGame()
+    {
+        numberOfClicks++;
+
+        if (numberOfClicks == 2)
+        {
+            SceneManager.LoadScene("Title Screen");
+        }
+    }
+}
