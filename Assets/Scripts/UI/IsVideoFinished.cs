4ee35acd40a3c1727eec56968e0749e5445ab643
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2020-12-20 03:26:15 +0100

    Finished with intro (code), and added new title screen

---
+++ Assets/Scripts/UI/IsVideoFinished.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.Video;
+using UnityEngine;
+
+public class IsVideoFinished : MonoBehaviour
+{
+
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+
+    private bool videoIsFinished;
+
+    private void Update()
+    {
+        CheckIfVideoIsFinished();
+
+        if (videoIsFinished)
+            gameObject.GetComponent<LoadThisScene>().LoadScene();
+    }
+
+
+    void CheckIfVideoIsFinished()
+    {
+        if ((ulong)videoPlayer.frame == videoPlayer.frameCount - 1)
+        {
+            videoIsFinished = true;
+        }
+    }
+
+}
