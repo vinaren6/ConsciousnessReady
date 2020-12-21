b5ab7ef421deef2b6093e5cbde39cde82cd1e8c1
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2020-12-21 10:38:34 -0800

    Fade in

---
+++ Assets/Scripts/UI/FadeIn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+public class FadeIn : MonoBehaviour
+{
+    private RawImage rend;
+
+    [SerializeField]
+    private float speed = 0.2f;
+
+    [SerializeField]
+    private float value = 1.4f;
+
+
+    private void Start()
+    {
+        rend = GetComponent<RawImage>();
+    }
+
+    private void FixedUpdate()
+    {
+        value -= Time.fixedDeltaTime * speed;
+
+        rend.color = new Color(0, 0, 0, value);
+        if (rend.color.a <= 0) {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
