9ab4abcafe011abf806c744453e00265d3d5d391
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2020-12-22 00:05:46 +0100

    Added draft of animated sanctuary outpost with heal

---
+++ Assets/Scripts/Hazards/DamageOnTouch.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DamageFromCrystals : MonoBehaviour
+{
+    [SerializeField]
+    private int damage = 40;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Health enemy = other.gameObject.GetComponent<Health>();
+
+        if (enemy != null)
+            enemy.TakeDamage(damage);
+    }
+
+}
---
+++ Assets/Scripts/Player/Heal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heal : MonoBehaviour
+{
+    [SerializeField]
+    private int healAmount = 5;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Health player = other.gameObject.GetComponent<Health>();
+
+        if (player != null)
+            player.Heal(healAmount);
+    }
+}
