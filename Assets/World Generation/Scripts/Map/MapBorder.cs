4886aa82b1b10bffb09bf77f101ee5344f70faf7
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2020-12-21 12:02:13 -0800

    Update MapBorder.cs

--- Assets/World Generation/Scripts/Map/MapBorder.cs
+++ Assets/World Generation/Scripts/Map/MapBorder.cs
@@ -17,18 +17,24 @@ public class MapBorder : MonoBehaviour
 
     private float size;
 
+    private Rigidbody2D player;
+
     void Start()
     {
         size = WorldGenerationHandler.instance.GetScale() + offset;
         GetComponent<CircleCollider2D>().radius = size;
         enabled = false;
         image.enabled = false;
+
+        player = PlayerMovement.playerObj.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        image.color = new Color(image.color.r, image.color.g, image.color.b, 
+        image.color = new Color(image.color.r, image.color.g, image.color.b,
             (Vector3.Distance(Vector3.zero, PlayerMovement.playerObj.transform.position) - size) / gradiantDist);
+
+        player.AddForce(-new Vector2((PlayerMovement.playerObj.transform.position.x) / (gradiantDist * 40f), (PlayerMovement.playerObj.transform.position.y) / (gradiantDist * 40f)));
     }
 
     private void OnTriggerExit2D(Collider2D collision)
