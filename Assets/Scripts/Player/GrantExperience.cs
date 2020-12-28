ef8b91a53d5fd0f0f8264306d5e10d19cb58172f
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2020-12-28 15:04:27 -0800

    added experiance point system

---
+++ Assets/Scripts/Player/GrantExperience.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GrantExperience : MonoBehaviour
+{
+    [SerializeField]
+    private int experiance = 1;
+    [SerializeField]
+    private int permanentExperiance = 0;
+
+    private bool haveBeenTrigered = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!haveBeenTrigered && collision.transform.CompareTag("Player")) {
+            haveBeenTrigered = true;
+            ExperiancePoints.Experiance += experiance;
+            ExperiancePoints.PermanentExperiance += permanentExperiance;
+        }
+    }
+
+}
