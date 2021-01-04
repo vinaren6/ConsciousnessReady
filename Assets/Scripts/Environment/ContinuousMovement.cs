0b0e4bb154acdacd4da6528052a6b16803315465
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2021-01-04 13:09:39 +0100

    Frozen Planet

---
+++ Assets/Scripts/Environment/ContinuousMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuousMovement : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.AddForce(new Vector2(Random.Range(750, 1500), Random.Range(750, 1500)));
+    }
+
+
+}
---
+++ Assets/Scripts/Environment/Orbit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private bool hasBeenMoved;
+
+
+    void Update()
+    {
+        if (!hasBeenMoved)
+        {
+            transform.Rotate(new Vector3(0f, 0f, speed) * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        hasBeenMoved = true;
+    }
+}
