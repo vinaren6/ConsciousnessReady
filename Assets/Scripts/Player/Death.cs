cd0f924d0aaaa6e8bf571b90a97496392ce92672
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-01-04 10:06:27 -0800

    created scripts for auto load on death

---
+++ Assets/Scripts/Player/Death.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] enable;
+    [SerializeField] GameObject[] desable;
+
+    private void OnDestroy()
+    {
+        foreach (GameObject obj in enable) {
+            obj.SetActive(true);
+        }
+        foreach (GameObject obj in desable) {
+            obj.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/LoadAfterDelay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAfterDelay : MonoBehaviour
+{
+    [SerializeField] string scene = "WorldGeneration";
+    [SerializeField] float delay = 10f;
+
+    void Start()
+    {
+        StartCoroutine(WaitAndPrint());
+    }
+
+
+    private IEnumerator WaitAndPrint()
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(scene);
+    }
+
+}
