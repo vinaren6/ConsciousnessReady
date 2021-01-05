d3ea0529f8877f3d6ea71b3f038eb0e743bc3b98
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2021-01-05 16:47:21 +0100

    Add tutorial, red planet cell, osv

--- Assets/Scripts/UI/Title Screen/TogglePanelVisibility.cs
+++ Assets/Scripts/UI/Title Screen/TogglePanelVisibility.cs
@@ -1,17 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TogglePanelVisibility : MonoBehaviour
 {
     [SerializeField]
     private GameObject panel;
 
+    [SerializeField]
+    private GameObject selectedButtonInPanel;
+
+    [SerializeField]
+    private GameObject selectedButtonWhenClosing;
+
     public void Display()
     {
         panel.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(selectedButtonInPanel);
     }
 
     public void Hide()
     {
         panel.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(selectedButtonWhenClosing);
     }
 }
