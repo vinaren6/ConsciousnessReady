41ea557031548b8b103561dbc803137901f5ea91
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2021-01-06 10:31:57 +0100

    Added starting cutscene

---
+++ Assets/IsCutsceneFinished.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class IsCutsceneFinished : MonoBehaviour
+{
+
+    [SerializeField]
+    private double switchToGameAtSecond;
+
+    [SerializeField]
+    private PlayableDirector playableDirector;
+
+    private void Update()
+    {
+        //CheckIfCutSceneIsFinished();
+
+        Debug.Log(playableDirector.time);
+
+        if (playableDirector.time > switchToGameAtSecond)
+            gameObject.GetComponent<LoadThisScene>().LoadScene();
+    }
+
+
+
+}
